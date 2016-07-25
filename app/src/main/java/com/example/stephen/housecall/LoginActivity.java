19e4611d8e4561ecd1edbf09a609a61e3769d560
tomli107 <tomli9999@hotmail.com>
2016-07-25 12:43:27 +0800

    First UI commit

---
+++ app/src/main/java/com/example/stephen/housecall/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.stephen.housecall;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public LoginActivity(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(),PatientMain.class);
+                startActivity(next);
+            }
+        });
+
+        Button doctorButton = (Button) findViewById(R.id.doctor_login_button);
+        doctorButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(),DoctorLogin.class);
+                startActivity(next);
+            }
+        });
+
+
+
+    }
+
+
+
+}
